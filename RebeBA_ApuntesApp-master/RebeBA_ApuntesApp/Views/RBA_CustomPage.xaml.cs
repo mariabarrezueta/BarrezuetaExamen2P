fd206aa9d551adb3a56113967516bc77bf46b320
María Rebeca Barrezueta Arrieta <maria.barrezueta@udla.edu.ec>
2024-06-05 11:39:15 -0500

    ingresar una cadena de caracteres alfanuméricos y calculos

--- RebeBA_ApuntesApp-master/RebeBA_ApuntesApp/Views/RBA_CustomPage.xaml.cs
+++ RebeBA_ApuntesApp-master/RebeBA_ApuntesApp/Views/RBA_CustomPage.xaml.cs
@@ -1,10 +1,48 @@
 using Microsoft.Maui.Controls;
-namespace RebeBA_ApuntesApp.Views;
+using System;
+using System.Linq;
 
-public partial class RBA_CustomPage : ContentPage
+namespace RebeBA_ApuntesApp.Views
 {
-	public RBA_CustomPage()
-	{
-		InitializeComponent();
-	}
+    public partial class RBA_CustomPage : ContentPage
+    {
+        public RBA_CustomPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculateClicked(object sender, EventArgs e)
+        {
+            string input = inputEntry.Text;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                resultLabel.Text = "Please enter a valid string.";
+                return;
+            }
+
+            int totalCharacters = input.Length;
+            int numberCount = input.Count(char.IsDigit);
+            int letterCount = input.Count(char.IsLetter);
+            int vowelCount = input.Count(c => "AEIOUaeiou".IndexOf(c) >= 0);
+            int uppercaseCount = input.Count(char.IsUpper);
+            int lowercaseCount = input.Count(char.IsLower);
+
+            resultLabel.Text = $"Total Characters: {totalCharacters}\n" +
+                               $"Numbers: {numberCount}\n" +
+                               $"Letters: {letterCount}\n" +
+                               $"Vowels: {vowelCount}\n" +
+                               $"Uppercase Letters: {uppercaseCount}\n" +
+                               $"Lowercase Letters: {lowercaseCount}";
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            inputEntry.Text = string.Empty;
+            resultLabel.Text = "Results will appear here";
+        }
+    }
 }
+
+
+
